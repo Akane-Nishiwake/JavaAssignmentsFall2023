c69ac4acfcc31cb12bdb53e7db4fafb86637f472
Amara Gitomer <59937361+Akane-Nishiwake@users.noreply.github.com>
2024-06-19 16:09:09 -0400

    Added Assignment 1 method call to main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        AssignmentMethods.Assignment01();
         AssignmentMethods.Assignment02();
         AssignmentMethods.Assignment03();
         AssignmentMethods.Assignment04(); //Output in data-sorted file
